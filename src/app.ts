42ae6b2425d6af274e2d67b2ed0953efb1b819ec
kristjan kangro <kristjan.kangro@visma.com>
2024-12-08 12:01:27 +0200

    section 11 done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import {ProjectInput} from "./Components/ProjectInput";
 import {ProjectList} from "./Components/ProjectList";
-
+console.log("test 333");
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
