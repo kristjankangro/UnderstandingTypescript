2b5eaeb1f1517a32effcc824c0f986b452ababfa
Kristjan <kristjan.kangro@visma.com>
2025-01-15 17:21:25 +0200

    5...

--- section05.classes.interfaces/src/app.ts
+++ section05.classes.interfaces/src/app.ts
@@ -1,25 +1,31 @@
-console.log("Starting application process");
-
 class Department {
-    // public name: string;
-    private employees: string[] = [];
+	// public name: string;
+	private employees: string[] = [];
 
-    constructor(private readonly id: number, public name: string) {
-    }
+	constructor(private readonly id: number, public name: string) {
+	}
 
-    describe(this: Department) {
-        console.log(this.name);
-    }
+	describe(this: Department) {
+		console.log(this.name);
+	}
 
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+	
 
-    printEmployeeCount() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
+	printEmployeeCount() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
+class ITDepartment extends Department {
+	constructor(id: number){
+		super(id, "IT osakond");
+	}
+}
+
+console.log("Starting application process");
 
 let d = new Department(1, "John Doe");
 console.log(d);
@@ -31,3 +37,7 @@ d.addEmployee("1 Doe");
 // dCopy.describe();
 
 d.printEmployeeCount();
+
+let d2 = new ITDepartment(2);
+
+d2.describe();
