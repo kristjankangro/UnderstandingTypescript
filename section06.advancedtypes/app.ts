f2b3245b7f7d1c4e8f187864827c4da5a5b6a2b9
kristjan kangro <kristjan.kangro@visma.com>
2025-01-19 18:45:46 +0200

    m86 done

--- section06.advancedtypes/app.ts
+++ section06.advancedtypes/app.ts
@@ -1,70 +1,23 @@
-type Admin = {
-	name: string;
-	privileges: string[];
-};
-
-interface Employee {
-	name: string;
-	startDate: Date;
+interface Bird {
+	type: "bird";
+	flyingSpeed: number;
 }
 
-// type ElevatedEmployee = Admin & Employee;
-interface ElevatedEmployee extends Admin, Employee {
+interface Horse {
+	type: "horse";
+	runningSpeed: number;
 }
 
-const e1: ElevatedEmployee = {
-	name: "Test",
-	privileges: ["ok"],
-	startDate: new Date("2021-01-12"),
+type Animal = Bird | Horse
+
+function move(a: Animal): void {
+	let speed: number;
+	if ("flyingSpeed" in a) speed = a.flyingSpeed;
+	if ("runningSpeed" in a) speed = a.runningSpeed;
+
+	console.log("moving with speed " + speed)
 }
 
-type Combinable = number | string;
+move({type: "horse", runningSpeed: 1});
 
-function add(a: Combinable, b: Combinable): Combinable {
-	return (typeof a === "number" && typeof b === "number") ? a + b :
-		(typeof a === "string" && typeof b === "number") ? a + b :
-			null;
-}
 
-type UnknownEmployee = Employee | Admin;
-
-//type guards
-function printEmployee(employee: UnknownEmployee) {
-	console.log(employee.name);
-	console.log(employee);
-	if ("privileges" in employee && employee.privileges) {
-		console.log(employee.privileges);
-	}
-}
-
-printEmployee({name: "Test", startDate: new Date("2021-01-12")});
-
-class Car {
-	drive() {
-		console.log("Drive");
-	}
-}
-
-class Truck {
-	drive() {
-		console.log("Drive truck!");
-	}
-
-	loadCargo() {
-		console.log("Load Cargo");
-	}
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-useVehicle(v1);
-useVehicle(v2);
-function useVehicle(v: Vehicle) {
-	v.drive();
-	if ("loadCargo" in v || v instanceof Truck) {
-		v.loadCargo();
-	}
-}
