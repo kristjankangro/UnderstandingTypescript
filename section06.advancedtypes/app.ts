ae38b8211008e0ff67c2f3d78f7bf3f70f98ed05
kristjan kangro <kristjan.kangro@visma.com>
2025-01-19 18:59:01 +0200

    m87 done

--- section06.advancedtypes/app.ts
+++ section06.advancedtypes/app.ts
@@ -1,23 +1,7 @@
-interface Bird {
-	type: "bird";
-	flyingSpeed: number;
-}
 
-interface Horse {
-	type: "horse";
-	runningSpeed: number;
-}
+//type casting
+const paragraph = document.querySelector("p");
+const el = document.getElementById("user-input") as HTMLInputElement;
 
-type Animal = Bird | Horse
-
-function move(a: Animal): void {
-	let speed: number;
-	if ("flyingSpeed" in a) speed = a.flyingSpeed;
-	if ("runningSpeed" in a) speed = a.runningSpeed;
-
-	console.log("moving with speed " + speed)
-}
-
-move({type: "horse", runningSpeed: 1});
-
+el.value = "Horse";
 
---
+++ section06.advancedtypes/module86.ts
@@ -0,0 +1,21 @@
+interface Bird {
+	type: "bird";
+	flyingSpeed: number;
+}
+
+interface Horse {
+	type: "horse";
+	runningSpeed: number;
+}
+
+type Animal = Bird | Horse
+
+function move(a: Animal): void {
+	let speed: number;
+	if ("flyingSpeed" in a) speed = a.flyingSpeed;
+	if ("runningSpeed" in a) speed = a.runningSpeed;
+
+	console.log("moving with speed " + speed)
+}
+
+move({type: "horse", runningSpeed: 1});
