b8fecfdb8b71af8e59cf584e609940198a2666ba
kristjan kangro <kristjan.kangro@visma.com>
2025-02-01 19:22:57 +0200

    generics done

--- section07.generics/app.ts
+++ section07.generics/app.ts
@@ -16,19 +16,19 @@ promise.then(data => data.split(" "))
 // module 96 functions
 //module 97 constraints
 
-function merge<T extends  object, U extends object>(o1: T, o2: U): T & U {
+function merge<T extends object, U extends object>(o1: T, o2: U): T & U {
 	return Object.assign(o1, o2);
 }
 
 const a = merge({name: "Test", startDate: new Date("2021-01-12")}, {age: 44});
 
-console.log(a.age)
+console.log("constraint " + a.age)
 
-interface WithLength{
+interface WithLength {
 	length: number;
 }
 
-function countAndPrintNoArr<T extends WithLength>(a: T) : [T, string] {
+function countAndPrintNoArr<T extends WithLength>(a: T): [T, string] {
 	let descrMessage = "aaa";
 	if (a.length < 1) {
 		console.log(descrMessage);
@@ -36,7 +36,7 @@ function countAndPrintNoArr<T extends WithLength>(a: T) : [T, string] {
 	return [a, descrMessage];
 }
 
-function countAndPrintArrow<T extends WithLength>(a: T) : [T, string] {
+function countAndPrintArrow<T extends WithLength>(a: T): [T, string] {
 	let descrMessage = "aaa";
 	if (a.length < 1) {
 		console.log(descrMessage);
@@ -44,7 +44,72 @@ function countAndPrintArrow<T extends WithLength>(a: T) : [T, string] {
 	return [a, descrMessage];
 }
 
-console.log(countAndPrintNoArr("111"));
-console.log(countAndPrintArrow("222"));
+console.log("generic func " + countAndPrintNoArr("111"));
+console.log("generic func " + countAndPrintArrow("222"));
 
+
+//module 99 key of
+const extractKeyOf = <T extends object, U extends keyof T>(obj: T, key: U) =>
+	"Value " + obj[key];
+
+// extractKeyOf({name: "Max"}, "age")
+extractKeyOf({name: "Max"}, "name")
+
+//module 100 generic classes
+class DataStorage<T> {
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		if (this.data.indexOf(item) == -1) return;
+		
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+
+	getList(): T[] {
+		return [...this.data];
+	}
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("a")
+textStorage.addItem("b")
+textStorage.addItem("c")
+textStorage.removeItem("b")
+
+console.log("Generic class: " + textStorage.getList());
+
+
+
+const objStorage = new DataStorage<object>();
+objStorage.addItem({name: "a"})
+objStorage.addItem({name: "b"})
+objStorage.addItem({name: "c"})
+objStorage.removeItem({name: "b"})
+
+console.log("Generic class: " + objStorage.getList());
+
+//module 102 generic utilities
+
+interface CourseGoal{
+	title: string;
+	description: string;
+	completeUntil: Date;
+}
+
+function createGoal(title: string, description: string, completeUntil: Date): CourseGoal {
+	let goal: Partial<CourseGoal> = {};
+	goal.title = title;
+	goal.description = description;
+	goal.completeUntil = completeUntil;
+	return goal as CourseGoal;
+}
+
+const names2: Readonly<string[]> = ["Max", "Sports"];
+// names2.push("Manu"); not allowed
+// names2.pop("Manu"); not allowed
+console.log(names2);
 
